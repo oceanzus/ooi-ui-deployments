2bddab82fd4463d5d4a6a0fb71c472949b551d07
James Case <case@oceanz.org>
2015-02-19 00:38:56 -0500

    Adds ooi-ui git-deploy.sh

---
+++ ooi-ui/git-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Sets the Git repo to deploy
+export DEPLOYMENT_REPO=https://github.com/asascience-open/ooi-ui
+# Sets the branch to deploy
+export DEPLOYMENT_BRANCH=master
+
+# --DO NOT MODIFY--
+cd /root
+wget $DEPLOYMENT_REPO/archive/$DEPLOYMENT_BRANCH.zip
+unzip $DEPLOYMENT_BRANCH.zip
+mv ooi-ui-$DEPLOYMENT_BRANCH ooi-ui
