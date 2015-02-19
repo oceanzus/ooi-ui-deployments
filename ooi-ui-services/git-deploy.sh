8fabc86fd7ecfbe4e80e6f475845cfa175488885
James Case <case@oceanz.org>
2015-02-18 23:41:40 -0500

    Another try.

---
+++ ooi-ui-services/git-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Sets the Git repo to deploy
+export DEPLOYMENT_REPO=https://github.com/asascience-open/ooi-ui-services
+# Sets the branch to deploy
+export DEPLOYMENT_BRANCH=master
+
+# --DO NOT MODIFY--
+cd /root
+wget $DEPLOYMENT_REPO/archive/$DEPLOYMENT_BRANCH.zip
+unzip $DEPLOYMENT_BRANCH.zip
+mv ooi-ui-services-$DEPLOYMENT_BRANCH ooi-ui-services
