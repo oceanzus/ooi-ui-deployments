290d89907fed35326736c0920a6025a7e79b658d
James Case <case@oceanz.org>
2015-02-21 09:37:52 -0500

    Fixes sed statements

--- ooi-ui/start_ooi-ui.sh
+++ ooi-ui/start_ooi-ui.sh
@@ -5,10 +5,10 @@ export PYTHONPATH=$PYTHONPATH:.
 sed -i -e 's/SERVICES_URL: http:\/\/localhost:4000/SERVICES_URL: http:\/\/ooiuiservices:4000/g' ooiui/config/config.yml
 
 # UI_API_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-sed -i -e "s/UI_API_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/UI_API_KEY: $UI_API_KEY/g" ooiui/config/config.yml
+sed -i -e "s|UI_API_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|UI_API_KEY: $UI_API_KEY|g" ooiui/config/config.yml
 
-# SECRET_KEY: 'ccdf5de08ac74855bda3e7e309d871e5'
-sed -i -e "s/SECRET_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/SECRET_KEY: '$SECRET_KEY'/g" ooiui/config/config.yml
+# SECRET_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+sed -i -e "s|SECRET_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|SECRET_KEY: $SECRET_KEY|g" ooiui/config/config.yml
 
 lip="$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"
 sed -i -e "s/localhost/$lip/g" app.py
