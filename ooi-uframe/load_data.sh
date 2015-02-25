58d6581635e3e7229744d82d28739f2242c29850
James Case <case@oceanz.org>
2015-02-25 15:13:04 -0500

    Adds full path to uframe

--- ooi-uframe/load_data.sh
+++ ooi-uframe/load_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd ~/
-source ~/.bash_profile
-source ~/ooi-cas/bin/edex-server
+cd /home/uframe/
+source /home/uframe/.bash_profile
+source /home/uframe/ooi-cas/bin/edex-server
 
-python /home/uframe/tmp/uframe_ingest/ingest/dataIngest.py -n 5 -d 1 -a -i /home/uframe/tmp/uframe_ingest/CP02PMCO/ingest_CP02PMCO_D00001.xlsx -e ~/ooi-cas/uframe-1.0/edex/
+python /home/uframe/tmp/uframe_ingest/ingest/dataIngest.py -n 5 -d 1 -a -i /home/uframe/tmp/uframe_ingest/CP02PMCO/ingest_CP02PMCO_D00001.xlsx -e /home/uframe/ooi-cas/uframe-1.0/edex/
 
-cp /home/uframe/tmp/uframe_ingest/CP02PMCO/Omaha_Cal_Info_CP02PMCO_00002.xlsx ~/ooi-cas/uframe-1.0/edex/data/ooi/asset_spreadsheet/
+cp /home/uframe/tmp/uframe_ingest/CP02PMCO/Omaha_Cal_Info_CP02PMCO_00002.xlsx /home/uframe/ooi-cas/uframe-1.0/edex/data/ooi/asset_spreadsheet/
--- ooi-uframe/start_uframe.sh
+++ ooi-uframe/start_uframe.sh
@@ -4,22 +4,22 @@ mkdir /home/uframe/tmp
 cp /home/uframe/source/ooi-cass_cd08b5e.tar.gz /home/uframe/tmp/
 tar -xzvf /home/uframe/tmp/ooi-cass_cd08b5e.tar.gz -C /home/uframe
 mv /home/uframe/ooi /home/uframe/ooi-cas
-source ~/ooi-cas/bin/edex-server
+source /home/uframe/ooi-cas/bin/edex-server
 
-cd ~/tmp
+cd /home/uframe/tmp
 cp /home/uframe/source/uframe_ingest.tar.gz /home/uframe/tmp/
 gunzip /home/uframe/tmp/uframe_ingest.tar.gz
 tar -xvf /home/uframe/tmp/uframe_ingest.tar
-cd ~/tmp/uframe_ingest
+cd /home/uframe/tmp/uframe_ingest
 gunzip /home/uframe/tmp/uframe_ingest/ingest_config_spreadsheets.tar.gz
 tar -xvf /home/uframe/tmp/uframe_ingest/ingest_config_spreadsheets.tar
 
-cd ~/
+cd /home/uframe/
 echo "export PATH=$PATH:/home/uframe/ooi-cas/uframe-1.0/java/bin" >> /home/uframe/.bash_profile
-source ~/.bash_profile
-source ~/ooi-cas/bin/edex-server
+source /home/uframe/.bash_profile
+source /home/uframe/ooi-cas/bin/edex-server
 
-/home/uframe/ooi-cas/uframe-1.0/cassandra/bin/cassandra -p ~/cass.pid &
+/home/uframe/ooi-cas/uframe-1.0/cassandra/bin/cassandra -p /home/uframe/cass.pid &
 
 /home/uframe/ooi-cas/bin/edex-server all start &
 
