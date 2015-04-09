fae937b5c6c0bbb76ab5e218c6f5e0edfb3c67c8
James Case <case@oceanz.org>
2015-04-09 10:19:44 -0400

    Reverts master to match master branches.

--- ooi-ui/git-deploy.sh
+++ ooi-ui/git-deploy.sh
@@ -2,7 +2,7 @@
 # Sets the Git repo to deploy
 export DEPLOYMENT_REPO=https://github.com/asascience-open/ooi-ui
 # Sets the branch to deploy
-export DEPLOYMENT_BRANCH=release4
+export DEPLOYMENT_BRANCH=master
 
 # --DO NOT MODIFY--
 cd /root
