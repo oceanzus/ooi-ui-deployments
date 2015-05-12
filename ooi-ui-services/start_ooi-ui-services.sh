f065be60fcbb996c1778ad6b09efb723ad2ecc87
James Case <case@oceanz.org>
2015-05-12 13:31:33 -0400

    Updates to release 5 configuration file structure.

--- ooi-ui-services/start_ooi-ui-services.sh
+++ ooi-ui-services/start_ooi-ui-services.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Check to make sure the input args have been passed in
-if [ -n "$DB_RESET" ] && [ -n "$DB_SAVE_USERS" ] && [ -n "$DB_NAME" ] && [ -n "$DB_HOST" ] && [ -n "$DB_USER" ] && [ -n "$DB_PASS" ] && [ -n "$DB_PORT" ] && [ -n "$DB_SCHEMA" ] && [ -n "$HOST_IP" ] && [ -n "$DEFAULT_ADMIN_USERNAME" ] && [ -n "$DEFAULT_ADMIN_PASSWORD" ] && [ -n "$DEPLOYMENT_SCENARIO" ]; then
+if [ -n "$DB_RESET" ] && [ -n "$DB_SAVE_USERS" ] && [ -n "$DB_NAME" ] && [ -n "$DB_HOST" ] && [ -n "$DB_USER" ] && [ -n "$DB_PASS" ] && [ -n "$DB_PORT" ] && [ -n "$DB_SCHEMA" ] && [ -n "$HOST" ] && [ -n "$DEFAULT_ADMIN_USERNAME" ] && [ -n "$DEFAULT_ADMIN_PASSWORD" ] && [ -n "$DEPLOYMENT_SCENARIO" ]; then
   echo "All parameters supplied...continuing..."
 else
   echo "Missing a launch parameter?"
-  echo "HOST_IP=$HOST_IP"
+  echo "HOST=$HOST"
   echo "DB_HOST=$DB_HOST"
   echo "DB_NAME=$DB_NAME"
   echo "DB_PORT=$DB_PORT"
@@ -30,33 +30,39 @@ workon ooiui
 export PYTHONPATH=$PYTHONPATH:.
 
 # Make the config files conform to the launch parameters
-sed -i -e "s|DEPLOYMENT_SCENARIO: LOCAL_DEVELOPMENT|DEPLOYMENT_SCENARIO: $DEPLOYMENT_SCENARIO|g" ooiservices/app/config.yml
-sed -i -e "s|HOST: localhost|HOST: $HOST_IP|g" ooiservices/app/config.yml
-sed -i -e "s|postgres://user:password@hostname/database_name|postgresql://$DB_USER:$DB_PASS@$DB_HOST/$DB_NAME|g" ooiservices/app/config.yml
-# SECRET_KEY: 'ccdf5de08ac74855bda3e7e309d871e5'
-sed -i -e "s|SECRET_KEY: 'ccdf5de08ac74855bda3e7e309d871e5'|SECRET_KEY: '$SECRET_KEY'|g" ooiservices/app/config.yml
-# UFRAME_URL: 'http://localhost:12575'
-sed -i -e "s|UFRAME_URL: 'http://localhost:12575'|UFRAME_URL: '$UFRAME_URL'|g" ooiservices/app/config.yml
-# UFRAME_ASSETS_URL: 'http://localhost:12573'
-sed -i -e "s|UFRAME_ASSETS_URL: 'http://localhost:12573'|UFRAME_ASSETS_URL: '$UFRAME_ASSETS_URL'|g" ooiservices/app/config.yml
-# UFRAME_URL_BASE: '/sensor/inv'
-sed -i -e "s|UFRAME_URL_BASE: '/sensor/inv'|UFRAME_URL_BASE: '$UFRAME_URL_BASE'|g" ooiservices/app/config.yml
-# UFRAME_DATA_REQUEST_LIMIT: 2880
-sed -i -e "s|UFRAME_DATA_REQUEST_LIMIT: 2880|UFRAME_DATA_REQUEST_LIMIT: '$UFRAME_DATA_REQUEST_LIMIT'|g" ooiservices/app/config.yml
-# REDMINE_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-sed -i -e "s|REDMINE_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|REDMINE_KEY: '$REDMINE_KEY'|g" ooiservices/app/config.yml
-# REDMINE_PROJECT_ID: "ooi-ui-api-testing"
-sed -i -e "s|REDMINE_PROJECT_ID: 'ooi-ui-api-testing'|REDMINE_PROJECT_ID: '$REDMINE_PROJECT_ID'|g" ooiservices/app/config.yml
-# UI_API_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-sed -i -e "s|UI_API_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|UI_API_KEY: $UI_API_KEY|g" ooiservices/app/config.yml
-# REDMINE_URL: 'https://uframe-cm.ooi.rutgers.edu'
-sed -i -e "s|REDMINE_URL: 'https://uframe-cm.ooi.rutgers.edu'|REDMINE_URL: '$REDMINE_URL'|g" ooiservices/app/config.yml
-# REDIS_URL: 'redis://:password@localhost:6379'
-sed -i -e "s|REDIS_URL: 'redis://:password@localhost:6379'|REDIS_URL: '$REDIS_URL'|g" ooiservices/app/config.yml
-# ENV_NAME: 'LOCAL_DEVELOPMENT'
 sed -i -e "s|ENV_NAME: 'LOCAL_DEVELOPMENT'|ENV_NAME: '$DEPLOYMENT_SCENARIO'|g" ooiservices/app/config.yml
-# USE_MOCK_DATA: False
+
+sed -i -e "s|LOGGING: True|LOGGING: $LOGGING|g" ooiservices/app/config.yml
+sed -i -e "s|LOG_FILE_PTAH: '/logs/'|LOG_FILE_PTAH: $LOG_FILE_PATH|g" ooiservices/app/config.yml
+sed -i -e "s|LOG_FILE: 'ooiservices.log'|LOG_FILE: $LOG_FILE|g" ooiservices/app/config.yml
+sed -i -e "s|SECRET_KEY: 'ccdf5de08ac74855bda3e7e309d871e5'|SECRET_KEY: '$SECRET_KEY'|g" ooiservices/app/config.yml
+sed -i -e "s|SSL_DISABLE: False|SSL_DISABLE: $SSL_DISABLE|g" ooiservices/app/config.yml
+sed -i -e "s|SQLALCHEMY_COMMIT_ON_TEARDOWN: True|SQLALCHEMY_COMMIT_ON_TEARDOWN: $SQLALCHEMY_COMMIT_ON_TEARDOWN|g" ooiservices/app/config.yml
+sed -i -e "s|SQLALCHEMY_RECORD_QUERIES: True|SQLALCHEMY_RECORD_QUERIES: $SQLALCHEMY_RECORD_QUERIES|g" ooiservices/app/config.yml
+sed -i -e "s|OOI_SLOW_DB_QUERY_TIME: 0.5|OOI_SLOW_DB_QUERY_TIME: $OOI_SLOW_DB_QUERY_TIME|g" ooiservices/app/config.yml
+sed -i -e "s|HOST: localhost|HOST: $HOST|g" ooiservices/app/config.yml
+sed -i -e "s|PORT: 4000|PORT: $PORT|g" ooiservices/app/config.yml
+sed -i -e "s|JSONIFY_PRETTYPRINT_REGULAR: true|JSONIFY_PRETTYPRINT_REGULAR: $JSONIFY_PRETTYPRINT_REGULAR|g" ooiservices/app/config.yml
+sed -i -e "s|UFRAME_URL: 'http://localhost:12576'|UFRAME_URL: '$UFRAME_URL'|g" ooiservices/app/config.yml
+sed -i -e "s|UFRAME_ASSETS_URL: 'http://localhost:12573'|UFRAME_ASSETS_URL: '$UFRAME_ASSETS_URL'|g" ooiservices/app/config.yml
+sed -i -e "s|UFRAME_URL_BASE: '/sensor/inv'|UFRAME_URL_BASE: '$UFRAME_URL_BASE'|g" ooiservices/app/config.yml
+sed -i -e "s|UFRAME_TOC: '/sensor/inv/toc'|UFRAME_TOC: $UFRAME_TOC|g" ooiservices/app/config.yml
+sed -i -e "s|UFRAME_INST_URL: 'http://uframe.ooi.rutgers.edu:12572'|UFRAME_INST_URL: $UFRAME_INST_URL|g" ooiservices/app/config.yml
+sed -i -e "s|UFRAME_INST_BASE: '/instrument/api'|UFRAME_INST_BASE: $UFRAME_INST_BASE|g" ooiservices/app/config.yml
+sed -i -e "s|UFRAME_PLAT_BASE: '/platform/api'|UFRAME_PLAT_BASE: $UFRAME_PLAT_BASE|g" ooiservices/app/config.yml
+sed -i -e "s|UFRAME_TIMEOUT_CONNECT: 5|UFRAME_TIMEOUT_CONNECT: $UFRAME_TIMEOUT_CONNECT|g" ooiservices/app/config.yml
+sed -i -e "s|UFRAME_TIMEOUT_READ: 300|UFRAME_TIMEOUT_READ: $UFRAME_TIMEOUT_READ|g" ooiservices/app/config.yml
+sed -i -e "s|REDMINE_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|REDMINE_KEY: '$REDMINE_KEY'|g" ooiservices/app/config.yml
+sed -i -e "s|UI_API_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|UI_API_KEY: $UI_API_KEY|g" ooiservices/app/config.yml
+sed -i -e "s|REDMINE_URL: 'https://uframe-cm.ooi.rutgers.edu'|REDMINE_URL: '$REDMINE_URL'|g" ooiservices/app/config.yml
+sed -i -e "s|REDMINE_PROJECT_ID: 'ooi-ui-api-testing'|REDMINE_PROJECT_ID: '$REDMINE_PROJECT_ID'|g" ooiservices/app/config.yml
+sed -i -e "s|WHOOSH_BASE: 'ooiservices/whoosh_index'|WHOOSH_BASE: $WHOOSH_BASE|g" ooiservices/app/config.yml
+sed -i -e "s|REDIS_URL: 'redis://:password@localhost:6379'|REDIS_URL: '$REDIS_URL'|g" ooiservices/app/config.yml
+sed -i -e "s|DEPLOYMENT_SCENARIO: LOCAL_DEVELOPMENT|DEPLOYMENT_SCENARIO: $DEPLOYMENT_SCENARIO|g" ooiservices/app/config.yml
+sed -i -e "s|UFRAME_DATA_REQUEST_LIMIT: 2880|UFRAME_DATA_REQUEST_LIMIT: '$UFRAME_DATA_REQUEST_LIMIT'|g" ooiservices/app/config.yml
 sed -i -e "s|USE_MOCK_DATA: False|USE_MOCK_DATA: '$USE_MOCK_DATA'|g" ooiservices/app/config.yml
+
+sed -i -e "s|postgres://user:password@hostname/database_name|postgresql://$DB_USER:$DB_PASS@$DB_HOST/$DB_NAME|g" ooiservices/app/config.yml
 
 # Reset the database
 if [ "$DB_INITIALIZE" == "True" ]; then
